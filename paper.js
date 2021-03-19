8f4411745b0d245aa4f786edc4eacc09e1194ee4
VidhitKore <77836256+VidhitKore@users.noreply.github.com>
2021-03-19 12:32:28 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,28 @@
+class paper {
+    constructor(x,y,radius) {
+    var options={
+    restitution:0.3,
+    friction:0,
+    density:1.2
+    }
+    this.x=x;
+    this.y=y;
+    this.radius=radius;
+    this.image=loadImage("paper.png");
+    this.body=Bodies.circle(this.x,this.y,(this.radius-20)/2,options);
+    World.add(world,this.body)
+
+    
+    }
+    display() {
+     var paperpos=this.body.position;
+     push();
+     Translate(paperpos.x,paperpos.y);
+     imageMode(CENTER);
+    image(this.image,0,0,this.radius,this.radius);
+    
+
+     
+    }
+} 
+
